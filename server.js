fec80a6eff789c03e94c72d3ef0d466dae34be00
cboswel1 <christianboswell86@gmail.com>
2020-11-14 15:29:29 -0700

    working server

---
+++ server.js
@@ -0,0 +1,19 @@
+//Dependencies 
+const express = require("express"); 
+const mongoose = require("mongoose"); 
+
+const PORT = process.env.PORT || 3080; 
+
+const app = express(); 
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true }); 
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
+  
