20d5c1a0cfae7a4778f57d782db4f3829dbdbd2c
cboswel1 <christianboswell86@gmail.com>
2020-11-16 18:21:21 -0700

    added toJSON virtual syntax to fix duration undefined issue

--- models/workout.js
+++ models/workout.js
@@ -1,42 +1,49 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: () => newDate(),
-  },
-  exercises: [
-    {
-      type: {
-        type: String,
-        trim: true,
-        required: "Exercise type",
-      },
-      name: {
-        type: String,
-        trim: true,
-        required: "Exercise name",
-      },
-      duration: {
-        type: Number,
-        trim: true
-      },
-      distance: {
-        type: Number,
-      },
-      weight: {
-        type: Number,
-      },
-      reps: {
-        type: Number,
-      },
-      sets: {
-        type: Number,
-      },
+const WorkoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: () => newDate(),
     },
-  ],
-});
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Exercise type",
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Exercise name",
+        },
+        duration: {
+          type: Number,
+          trim: true,
+        },
+        distance: {
+          type: Number,
+        },
+        weight: {
+          type: Number,
+        },
+        reps: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
 
 // PersonSchema
 // .virtual('name.full')
@@ -44,10 +51,10 @@ const WorkoutSchema = new Schema({
 //   return this.name.first + ' ' + this.name.last;
 // })
 
-WorkoutSchema.virtual("totalDuration").get(function() {
-    return this.exercises.reduce((total, exercise) => {
-        return total + exercise.duration;
-    }, 0);
+WorkoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
--- server.js
+++ server.js
@@ -1,33 +1,31 @@
-//Dependencies 
-const express = require("express"); 
-const mongoose = require("mongoose"); 
+//Dependencies
+const express = require("express");
+const mongoose = require("mongoose");
 const morgan = require("morgan");
 
 //Api Routes
-const apiRoutes = require("./routes/apiRoutes"); 
-const htmlRoutes = require("./routes/htmlRoutes")
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
-const PORT = process.env.PORT || 3080; 
+const PORT = process.env.PORT || 3080;
 
-const app = express(); 
+const app = express();
 
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.use(htmlRoutes); 
+app.use(htmlRoutes);
 app.use(apiRoutes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
-{ 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-}); 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-  });
-  
+  console.log(`App running on port ${PORT}!`);
+});
