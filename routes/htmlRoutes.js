ef2cef09fae493b2b4be2bfdfab43bf578539888
cboswel1 <christianboswell86@gmail.com>
2020-11-17 18:52:51 -0700

    changed routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,11 +5,11 @@ const router = express.Router();
 const path = require("path");
 
 router.get("/exercise", function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  res.sendFile(path.join(__dirname, "../public/exercise"));
 });
 
 router.get("/stats", function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/stats.html"));
+  res.sendFile(path.join(__dirname, "../public/stats"));
 });
 
 module.exports = router;
